1f2a0f5e0fa96b85b93d4b62530b42599b2efe9d
Denis Asaba <deniaasaba8@gmail.com>
2026-07-03 15:40:16 -0400

    app.py

--- app.py
+++ app.py
@@ -11,3 +11,15 @@ print(name)
 
 is_student = True
 print(is_student)
+
+count = 10
+price = 19.99
+message = "The total price is: "
+is_available = False
+value = None
+
+print(count, type(count))
+print(price, type(price))
+print(message, type(message))
+print(is_available, type(is_available))
+print(value, type(value))
