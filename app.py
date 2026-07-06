27cc1e242a224d699aed35da7f3d2eb735e96141
Denis Asaba <deniaasaba8@gmail.com>
2026-07-06 16:36:48 -0400

    app.py

--- app.py
+++ app.py
@@ -23,3 +23,39 @@ print(price, type(price))
 print(message, type(message))
 print(is_available, type(is_available))
 print(value, type(value))
+
+print(8 * 60)
+
+hours = 8
+minutes_per_hour = 60
+total_minutes = hours * minutes_per_hour
+print(total_minutes)
+
+hours = 10
+minutes = hours * minutes_per_hour
+print(minutes)
+
+x = 5.99
+y = .06664
+z = x * y
+t = x + y
+print(z)
+print(t)
+
+subtotal = 100
+tax_rate = 0.07
+tax_amount = subtotal * tax_rate
+total = subtotal + tax_amount
+print(total)
+
+city = "New York"
+state = "NY"
+temp = 75
+is_raining = False
+nickname = None
+
+print(city, type(city))
+print(state, type(state))
+print(temp, type(temp))
+print(is_raining, type(is_raining))
+print(nickname, type(nickname))
